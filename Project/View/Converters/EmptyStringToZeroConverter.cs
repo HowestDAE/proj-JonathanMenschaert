ad0112aa3962ac6ee2a88d5805c4499a7471c40b
Jonathan M <jonathan.658@telenet.be>
2023-04-07 01:02:35 +0200

    Updated Detailpage design
    Reworked the detailpage design to better fit the design of the overviewpage

---
+++ Project/View/Converters/EmptyStringToZeroConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Project.View.Converters
+{
+    public class EmptyStringToZeroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string strVal = value.ToString();
+            if (string.IsNullOrEmpty(strVal))
+            {
+                return "0";
+            }
+            else
+            {
+                return strVal;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
